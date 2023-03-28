6340a0c2cf6d217d4baaeab740f5d365c342cb65
Kist <kistaaa@users.noreply.github.com>
2023-03-27 23:26:52 -0300

    Create Python-midi-notes.py

---
+++ Python-midi-notes.py
@@ -0,0 +1,20 @@
+from mido import MidiFile
+tempo=0
+noteons = []
+notas = []
+mid = MidiFile('C:/scripts-python/meow.mid')
+
+for evt in mid:
+    if not evt.is_meta:
+        tempo += evt.time
+        if evt.type == 'note_on':
+            noteons.append(evt)
+            
+        elif evt.type == 'note_off':
+            for nota in noteons:
+                if nota.note == evt.note:
+                    com = tempo - evt.time
+                    fim = tempo
+                    dur = fim - com
+                    notas.append([evt.note, com, fim, dur])
+                    noteons.remove(nota)
